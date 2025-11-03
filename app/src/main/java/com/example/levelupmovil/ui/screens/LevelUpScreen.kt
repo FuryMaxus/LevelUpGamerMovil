39822a183479e37d352ffdad73dbc71220b551c8
Jose Valenzuela <jivalenzuela2010@hotmail.com>
2025-11-03 19:09:05 -0300

    movimiento de lineas en el codigo

--- app/src/main/java/com/example/levelupmovil/ui/screens/LevelUpScreen.kt
+++ app/src/main/java/com/example/levelupmovil/ui/screens/LevelUpScreen.kt
@@ -2,22 +2,15 @@ package com.example.levelupmovil.ui.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import com.example.levelupmovil.viewmodel.MainViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen() {
+fun LevelUpScreen() {
     Column(modifier = Modifier.fillMaxSize()) {
-        Text("Home")
+        Text("LevelUp")
     }
 }
